6ea18b7102bef30aa23eb850ead6e9a4e157388f
oussama <osm2014@hotmail.fr>
2023-06-14 19:01:49 +0100

    make blog

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.jsx";
+import Main from "./components/Main.jsx";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div>
+          <Navbar />
+            <Main />
+      </div>
+    </>
+  );
+}
+
+export default App;
